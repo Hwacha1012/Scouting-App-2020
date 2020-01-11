5bd2e6dccfea5d01ab46a3d362ecabbca6367399
Jing Liu <chesterandchu@gmail.com>
2020-01-10 19:38:47 -0500

    Initial Commit

---
+++ ScoutingApp2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScoutingApp2020
+//
+//  Created by Leonard Liu on 1/10/20.
+//  Copyright © 2020 Leonard Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ScoutingApp2020Tests/ScoutingApp2020Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ScoutingApp2020Tests.swift
+//  ScoutingApp2020Tests
+//
+//  Created by Leonard Liu on 1/10/20.
+//  Copyright © 2020 Leonard Liu. All rights reserved.
+//
+
+import XCTest
+@testable import ScoutingApp2020
+
+class ScoutingApp2020Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
