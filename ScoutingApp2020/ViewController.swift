24b149e553bb470c67d093c1e96087201232bcef
Jing Liu <chesterandchu@gmail.com>
2020-01-10 19:50:14 -0500

    Added Image

--- ScoutingApp2020/ViewController.swift
+++ ScoutingApp2020/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
+UIColor teamColor = UIColor.init(red: 0.0431372549, green: 0.1294117647, blue: 0.50196078431, alpha: 1.0)
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //hi
+    
     }
 
 
