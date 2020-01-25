11328c2fa526f99ad0c8d2fe37381578133b4db9
Najeed <Swifter11110@gmail.com>
2020-01-25 13:37:32 -0500

    Added new files and such for pit scouting

---
+++ ScoutingApp2020/PitScoutingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PitScoutingViewController.swift
+//  ScoutingApp2020
+//
+//  Created by ghsrobotics on 1/25/20.
+//  Copyright © 2020 Leonard Liu. All rights reserved.
+//
+
+import UIKit
+
+class PitScoutingViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
