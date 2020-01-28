fc2b4d8a6056d82b86227cdb53cbb4ed0d4fd552
Leo Liu <leoqh6@gmail.com>
2020-01-28 17:31:32 -0500

    Added finishMatchViewController

---
+++ ScoutingApp2020/FinishMatchDataViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FinishMatchDataViewController.swift
+//  ScoutingApp2020
+//
+//  Created by Leonard Liu on 1/28/20.
+//  Copyright © 2020 Leonard Liu. All rights reserved.
+//
+
+import UIKit
+
+class FinishMatchDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
