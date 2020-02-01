dd6960bd296d4943750831fc1f66e6811862cfeb
Najeed <Swifter11110@gmail.com>
2020-02-01 14:55:37 -0500

    Added all the saving and changing to JSON stuff (With the help of Alex I'm not that smart)

--- ScoutingApp2020/PitScoutingViewController.swift
+++ ScoutingApp2020/PitScoutingViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class PitScoutingViewController: UIViewController {
+import Foundation
 
+class PitScoutingData: Codable {
+   public var robotNumber:String
+    public var driveTrainType:String
+    public var intake:String
+    public var capacity:String
+    public var AutoLineCrossing:Bool
+    public var AutoHighBalls:String
+    public var AutoLowBalls:String
+    public var climb:Bool
+    public var notes:String
+    init(robotNumber: String, driveTrainType:String, intake:String, capacity:String, AutoLineCrossing:Bool, AutoHighBalls:String, AutoLowBalls:String, climb:Bool, notes:String ) {
+        self.driveTrainType = driveTrainType
+        self.robotNumber = robotNumber
+        self.intake = intake
+        self.capacity = capacity
+        self.AutoLineCrossing = AutoLineCrossing
+        self.AutoHighBalls = AutoHighBalls
+        self.AutoLowBalls = AutoLowBalls
+        self.climb = climb
+        self.notes = notes
+    }
+}
+
+class PitScoutingViewController: UIViewController {
+    
+    public var pitScoutingDataObj = PitScoutingData(robotNumber: "", driveTrainType:"Tank", intake:"Floor", capacity:"1", AutoLineCrossing:true, AutoHighBalls:"", AutoLowBalls:"", climb:true, notes:"")
+    
+    
     @IBOutlet weak var RobotNumber: UITextField!
     
     
@@ -32,9 +60,141 @@ class PitScoutingViewController: UIViewController {
     
     @IBOutlet weak var Next: UIButton!
     
+    
+    
+    
+    @IBOutlet weak var Climbing: UISwitch!
+    func Serialize(teamNumber:String, driveTrainType:String, intake:String, capacity:String, AutoLineCrossing:Bool, AutoHighBalls:String, AutoLowBalls:String, climb:Bool, notes:String,pretty:Bool)
+    {
+       pitScoutingDataObj = PitScoutingData(robotNumber: teamNumber, driveTrainType:driveTrainType, intake:intake, capacity:capacity, AutoLineCrossing:AutoLineCrossing, AutoHighBalls:AutoHighBalls, AutoLowBalls:AutoLowBalls, climb:climb, notes:notes)
+        let encoder = JSONEncoder()
+        if (pretty == true)
+        {
+        encoder.outputFormatting = .prettyPrinted // if necessary
+        }
+        let data = try! encoder.encode(pitScoutingDataObj)
+        let jsonString = String(data: data, encoding: .utf8)!
+        print(jsonString)
+    }
+    func Deserialize(jsonString:String) ->
+        PitScoutingData{
+        let jsonData = jsonString.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        pitScoutingDataObj = try! decoder.decode(PitScoutingData.self, from: jsonData);
+        return dump(pitScoutingDataObj)
+    }
+    
+    func validateNumber(text: String) -> Bool {
+        var result = false
+        // test password
+           let num = Int(text);
+
+           if num != nil {
+              result = true
+           }
+        return result
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+      print("You may enter")
+
+    }
+    
+    @IBAction func robotNumber_Changed(_ sender: Any) {
+        if(validateNumber(text: RobotNumber.text!)) {
+            // correct password
+            print("Success.");
+           RobotNumber.backgroundColor = UIColor.green
+            pitScoutingDataObj.robotNumber = RobotNumber.text!
+
+        } else {
+            print("Failed");
+            RobotNumber.backgroundColor = UIColor.red
+            
+        }
+    }
+    
+    
+
+    @IBAction func driveTrainType_Selected(_ sender: Any) {
+        let title = DrivetrainType.titleForSegment(at: DrivetrainType.selectedSegmentIndex)
+        pitScoutingDataObj.driveTrainType = title!
+    }
+    
+    @IBAction func intake_Selected(_ sender: Any) {
+
+        let title = Intake.titleForSegment(at: Intake.selectedSegmentIndex)
+        pitScoutingDataObj.intake = title!
+    }
+    
+    
+    @IBAction func capacity_Selected(_ sender: Any) {
+        let title = Capacity.titleForSegment(at: Capacity.selectedSegmentIndex)
+        pitScoutingDataObj.capacity = title!
+    }
+    
+    
+    @IBAction func autoLineCrossing_Selected(_ sender: Any) {
+        pitScoutingDataObj.AutoLineCrossing = AutoLineCrossing.isOn
+    }
+    
+    @IBAction func Climbing(_ sender: Any) {
+        pitScoutingDataObj.climb = Climbing.isOn
+    }
+    
+    @IBAction func AutoHighGoalBalls_Changed(_ sender: Any) {
+        if(validateNumber(text: AutoHighGoalBalls.text!)) {
+            // correct password
+            print("Success.");
+           AutoHighGoalBalls.backgroundColor = UIColor.green
+
+        } else {
+            print("Failed");
+            AutoHighGoalBalls.backgroundColor = UIColor.red
+            
+        }
+        
+    }
+    
+    
+    @IBAction func autoLowBalls_Changed(_ sender: Any) {
+        if(validateNumber(text: AutoLowGoalBalls.text!)) {
+            // correct password
+            print("Success.");
+           AutoLowGoalBalls.backgroundColor = UIColor.green
+
+        } else {
+            print("Failed");
+            AutoLowGoalBalls.backgroundColor = UIColor.red
+            
+    }
+   
+    }
+    //The thing
+
+
+    
+    @IBAction func submit(_ sender: Any) {
+        
+
+        let payload =  Serialize(
+               teamNumber: pitScoutingDataObj.robotNumber,
+               driveTrainType: pitScoutingDataObj.driveTrainType,
+               intake: pitScoutingDataObj.intake,
+               capacity: pitScoutingDataObj.capacity,
+               AutoLineCrossing: pitScoutingDataObj.AutoLineCrossing,
+               AutoHighBalls: pitScoutingDataObj.AutoHighBalls,
+               AutoLowBalls: pitScoutingDataObj.AutoLowBalls,
+               climb: pitScoutingDataObj.climb,
+               notes: pitScoutingDataObj.notes,
+               pretty: false)
+           print(payload)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+            
+        
         // Do any additional setup after loading the view.
     }
     
