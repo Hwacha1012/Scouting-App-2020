3a43723054bdbea8a02363b6505d398f17a83345
Leo Liu <leoqh6@gmail.com>
2020-02-15 10:58:38 -0500

    Made segue from tableView to matchData and fixed the link to test opening urls

--- ScoutingApp2020/MatchDataViewController.swift
+++ ScoutingApp2020/MatchDataViewController.swift
@@ -58,25 +58,109 @@ class MatchDataViewController: UIViewController {
             autoButtons.titleLabel?.font = UIFont.init(name: "Optima-BoldItalic", size: 35 )
         }
         
-        
-        if (matchPosition > 1){
-            //load previous data
-            update()
+        if(changeData){
+            //entered viewController from viewData
+            let defaults = UserDefaults.standard
+            
+            if(teamNumberFromButton.contains("PitScouting")){
+                if UserDefaults.standard.array(forKey: "pitScoutList") != nil{
+                    pitScoutList = UserDefaults.standard.object(forKey: "pitScoutList") as! [String]
+                    print("\(pitScoutList) and \(pitScoutList.count)")
+                               
+                }
+                else{
+                    print("pitScoutList is nil")
+                }
+                var counter = 0
+                
+                for i in 0...pitScoutList.count - 1{
+                    if(teamNumberFromButton == pitScoutList[i]){
+                        counter = i
+                        break
+                    }
+                }
+                //print(pitScoutList[counter])
+                /*
+                
+                var data = Deserialize(jsonString: defaults.object(forKey: pitScoutList[counter]) as! String)
+                RobotNumber.text = data.robotNumber
+                RobotNumber.backgroundColor = UIColor.green
+                print("driveTrain type is \(data.driveTrainType)")
+                if(data.driveTrainType == "Tank"){
+                    DrivetrainType.selectedSegmentIndex = 0
+                }
+                else if(data.driveTrainType == "Mechanum"){
+                    DrivetrainType.selectedSegmentIndex = 1
+                }
+                else if(data.driveTrainType == "H-Drive"){
+                    DrivetrainType.selectedSegmentIndex = 2
+                }
+                else if(data.driveTrainType == "Swerve"){
+                    DrivetrainType.selectedSegmentIndex = 3
+                }
+                if(data.intake == "Floor"){
+                    Intake.selectedSegmentIndex = 0
+                
+                }
+                else{
+                    Intake.selectedSegmentIndex = 1
+                }
+                
+                Capacity.selectedSegmentIndex = (Int(data.capacity) ?? 1) - 1
+                AutoLineCrossing.isOn = data.AutoLineCrossing
+                AutoHighGoalBalls.text = data.AutoHighBalls
+                AutoHighGoalBalls.backgroundColor = UIColor.green
+                AutoLowGoalBalls.text = data.AutoLowBalls
+                AutoLowGoalBalls.backgroundColor = UIColor.green
+                Climbing.isOn = data.climb
+                Notes.text = data.notes
+                */
+            }
+            else{
+                if UserDefaults.standard.array(forKey: "teamList") != nil{
+                    teamList = UserDefaults.standard.object(forKey: "teamList") as! [String]
+                   // print("\(teamList) and \(teamList.count)")
+                    
+                    var counter = 0
+                    
+                    for i in 0...teamList.count - 1{
+                        if(teamNumberFromButton == teamList[i]){
+                            counter = i
+                            break
+                        }
+                    }
+                    //var data = Deserialize(jsonString: defaults.object(forKey: pitScoutList[counter]) as! String)
+                    
+                    
+                    
+                }
+                else{
+                    print("teamList is nil")
+                }
+            }
+
+            
+            
         }
         else{
-            //have new data
-            
-            scoutName = ""
-            teamNumber = 0
-            matchNumber = 0
-            teamColor = true
-            colorSegmentedControl.selectedSegmentTintColor = UIColor.blue
-            //colorSegmentedControl.selectedSegmentTintColor
-            //colorSegmentedControl.tintColor =
-            
+            if (matchPosition > 1){
+                //load previous data
+                update()
+            }
+            else{
+                //have new data
+                
+                scoutName = ""
+                teamNumber = 0
+                matchNumber = 0
+                teamColor = true
+                colorSegmentedControl.selectedSegmentTintColor = UIColor.blue
+                
+            }
         }
         
-        //colorSegmentedControl.
+        
+
         
     }
     
