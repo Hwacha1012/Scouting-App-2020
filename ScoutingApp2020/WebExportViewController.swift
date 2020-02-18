5a7e798ce553c6ab1ac2c130ef9f88a538c11a3c
Najeed <Swifter11110@gmail.com>
2020-02-18 18:16:12 -0500

    this one sends the string properly. I finally made it put of leos basement

--- ScoutingApp2020/WebExportViewController.swift
+++ ScoutingApp2020/WebExportViewController.swift
@@ -12,7 +12,7 @@ class WebExportViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
